b89ad8026a58c2178ddd73c4a513cc5cd45f25a1
ad-world <67097683+ad-world@users.noreply.github.com>
2022-04-30 13:36:12 +0400

    Add props to turd button row

--- src/components/TurdButtonRow/TurdButtonRow.jsx
+++ src/components/TurdButtonRow/TurdButtonRow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col} from "react-bootstrap"
 import "./TurdButtonRow.styles.css"
 import brownPoop from "../../images/brownpoop.png";
@@ -7,29 +7,30 @@ import purplePoop from "../../images/purplepoop.png";
 import redPoop from "../../images/redpoop.png";
 import whitePoop from "../../images/whitepoop.png"
 
-function TurdButtonRow(){    
+function TurdButtonRow(props){    
+
     return (
         <Row className="mx-auto justify-content-center gx-0">
             <Col xs={2} className="text-center">
-                <button className="m-2 poop">
+                <button className="m-2 poop" onClick={() => props.choosePoop("brown")}>
                     <img src={brownPoop} className="image"></img>
                 </button>
             </Col>
             <Col xs={2} className="text-center">
-                <button className="m-2 poop">
+                <button className="m-2 poop" onClick={() => props.choosePoop("green")}>
                     <img src={greenPoop} className="image"></img>
                 </button>
             </Col>
             <Col xs={2} className="text-center">
-                <button className="m-2 poop">
+                <button className="m-2 poop" onClick={() => props.choosePoop("purple")}>
                     <img src={purplePoop} className="image"></img>
                 </button></Col>
             <Col xs={2} className="text-center">
-                <button className="m-2 poop">
+                <button className="m-2 poop" onClick={() => props.choosePoop("red")}>
                    <img src={redPoop} className="image"></img>
                 </button></Col>
             <Col xs={2} className="text-center">
-                <button className="m-2 poop">
+                <button className="m-2 poop" onClick={() => props.choosePoop("white")}>
                     <img src={whitePoop} className="image"></img>
                 </button>
             </Col>
