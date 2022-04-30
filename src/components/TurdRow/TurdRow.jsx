1b4d3949980ae9e5d0049766896004b93bfc28aa
ad-world <67097683+ad-world@users.noreply.github.com>
2022-04-30 14:22:52 +0400

    Show colors in each cell

--- src/components/TurdRow/TurdRow.jsx
+++ src/components/TurdRow/TurdRow.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Container, Row, Col} from "react-bootstrap"
 import "./TurdRow.styles.css"
 
-function TurdRow(){    
+function TurdRow(props){    
+    const turd = props.turd;
+    console.log(turd);
     return (
         <Container>
             <Row>
-                <Col className="cell m-2"></Col>
-                <Col className="cell m-2"></Col>
-                <Col className="cell m-2"></Col>
-                <Col className="cell m-2"></Col>
-                <Col className="cell m-2"></Col>
+                <Col className="cell m-2">{turd[0]}</Col>
+                <Col className="cell m-2">{turd[1]}</Col>
+                <Col className="cell m-2">{turd[2]}</Col>
+                <Col className="cell m-2">{turd[3]}</Col>
+                <Col className="cell m-2">{turd[4]}</Col>
             </Row>
         </Container>
     )
