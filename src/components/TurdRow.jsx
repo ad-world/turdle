080f8693f5ed6f46741c53ff0ef2493c8502b894
ad-world <67097683+ad-world@users.noreply.github.com>
2022-04-30 05:05:29 +0400

    Create turd grid

---
+++ src/components/TurdRow.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Row, Col} from "react-bootstrap"
+import "./TurdRow.styles.css"
+
+function TurdRow(){    
+    return (
+        
+        <Container>
+            <Row>
+                <Col className="cell m-2"></Col>
+                <Col className="cell m-2"></Col>
+                <Col className="cell m-2"></Col>
+                <Col className="cell m-2"></Col>
+                <Col className="cell m-2"></Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default TurdRow
