4d27546fadc363fcf0fef3c650bd51bec4f5fdee
ad-world <67097683+ad-world@users.noreply.github.com>
2022-04-30 14:23:03 +0400

    Add onclick props to UtilBar

--- src/components/UtilBar/UtilBar.jsx
+++ src/components/UtilBar/UtilBar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap"
 import "./UtilBar.styles.css"
 
-function UtilBar() {
+function UtilBar(props) {
     return(
         <Row className="mx-auto">
             <Col className="mx-auto text-center m-2">
-                <button className="btnrow">
+                <button className="btnrow" onClick={() => props.guess()}>
                     Try Guess
                 </button>
             </Col>
             <Col className="mx-auto text-center m-2"> 
-                <button className="btnrow">
+                <button className="btnrow" onClick={() => props.delete()}>
                     Delete
                 </button>
             </Col>
