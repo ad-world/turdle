4cbacf193601e624ec2dd1efbc3054a5d9b23e3f
ad-world <67097683+ad-world@users.noreply.github.com>
2022-05-01 08:24:45 +0400

    Add winner modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import "react-bootstrap"
 import Header from './components/Header/Header'
 import Grid from "./components/Grid/Grid";
+import WinModal from './components/WinModal/WinModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import useHash from './util/useHash';
 import moment from "moment"
 
 function App() {
+  const [win, setWin] = useState(false);
+  const [winRow, setWinRow] = useState(0);
 
   const startDate = new Date(2022, 3, 29);
   const currentDate = new Date();
@@ -16,12 +19,20 @@ function App() {
 
   const guess = useHash(formattedDate);
   
-  const day = currentDate.getDate() - startDate.getDate();
+  const diff = currentDate.getTime() - startDate.getTime();
+  const day = Math.ceil(diff / (1000 * 3600 * 24));
+
+  const winner = (row) => {
+    setWin(true);
+    setWinRow(row);
+  }
+
 
   return (
     <div className="App">
       <Header></Header>
-      <Grid guess={guess}></Grid>
+      <Grid guess={guess} win={winner}></Grid>
+      { win && <WinModal attemps={winRow} day={day} win={win}></WinModal>}
     </div>
   )
 }
