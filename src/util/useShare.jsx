7486b5dc0d9943146c44a8c36e976ae1409516dc
ad-world <67097683+ad-world@users.noreply.github.com>
2022-05-02 09:42:20 +0400

    Create useShare hook

---
+++ src/util/useShare.jsx
@@ -0,0 +1,26 @@
+function useShare(rows, day, win) {
+    rows = rows.filter(item => item.length != 0);
+    const length = rows.length;
+    var message = `Turdle Day ${day} 💩 \n\n ${length}/6\n`;
+    for(var i = 0; i < length; i++) {
+        for(var j = 0; j < rows[i].length; j++) {
+            if(rows[i][j] == 0) {
+                message += '⬜';
+            } else if(rows[i][j] == 1) {
+                message += '🟨';
+            } else if(rows[i][j] == 2) {
+                message += '🟩';
+            }
+        }
+        if(i != length - 1) {
+            message += '\n'; 
+        }
+    }
+    if(win) {
+        return message;
+    } else {
+        return `Turdle Day ${day} 💩 \n\n X/6`;
+    }
+}
+
+export default useShare
