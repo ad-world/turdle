3b55d41ce2af1dc7a92ff2d0c08c19b4e068e07f
ad-world <67097683+ad-world@users.noreply.github.com>
2023-08-28 20:05:46 +0400

    Update base in vite config

--- src/components/TurdImage/TurdImage.jsx
+++ src/components/TurdImage/TurdImage.jsx
@@ -3,31 +3,21 @@ import brownPoop from "../../images/brownpoop.png";
 import greenPoop from "../../images/greenpoop.png";
 import purplePoop from "../../images/purplepoop.png";
 import redPoop from "../../images/redpoop.png";
-import whitePoop from "../../images/whitepoop.png"
-import "./TurdImage.styles.css"
+import whitePoop from "../../images/whitepoop.png";
+import "./TurdImage.styles.css";
 
 function TurdImage(props) {
-    if(props.color == "green") {
-        return (
-            <img src={greenPoop} className="image2"></img>
-        )
-    } else if(props.color == "red") {
-        return (
-            <img src={redPoop} className="image2"></img>
-        )
-    } else if(props.color == "brown") {
-        return (
-            <img src={brownPoop} className="image2"></img>
-        )
-    } else if(props.color == "white") {
-        return (
-            <img src={whitePoop} className="image2"></img>
-        )
-    } else if(props.color == "purple") {
-        return (
-            <img src={purplePoop} className="image2"></img>
-        )
-    }
+  if (props.color == "green") {
+    return <img src={greenPoop} className="image2"></img>;
+  } else if (props.color == "red") {
+    return <img src={redPoop} className="image2"></img>;
+  } else if (props.color == "brown") {
+    return <img src={brownPoop} className="image2"></img>;
+  } else if (props.color == "white") {
+    return <img src={whitePoop} className="image2"></img>;
+  } else if (props.color == "purple") {
+    return <img src={purplePoop} className="image2"></img>;
+  }
 }
 
 export default TurdImage;
